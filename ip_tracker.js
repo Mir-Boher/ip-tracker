a673e2e52a6286ed4cdfc42d4146de4e3c7f91b4
Mir-Boher <bohermir@yahoo.com>
2025-08-30 21:01:19 +0500

    Moved zoom in/out at the bottom right

--- ip_tracker.js
+++ ip_tracker.js
@@ -42,7 +42,11 @@ function initializeMap() {
   if (map) {
     map.remove();
   }
-  map = L.map("map").setView(defaultPosition, defaultZoom);
+  map = L.map("map", { zoomControl: false }).setView(
+    defaultPosition,
+    defaultZoom
+  );
+  L.control.zoom({ position: "bottomright" }).addTo(map);
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
